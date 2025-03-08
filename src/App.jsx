ee33b2cb98fc96a21f86c1e286cda86875bef597
Emirhan Boyaci <6002029@mborijnland.nl>
2025-03-08 23:27:35 +0100

    testing bootstrap etc.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import './styles/App.css';
 
 function App() {
     return (
-        <div className="App">
+        <div className="App container">
             <header className="App-header">
                 <h1>Alpaca Image Generator</h1>
             </header>
-            <main>
-                {/* Main content will go here */}
+            <main className='col-12 col-md-8 col-lg-6 mx-auto'>
+
             </main>
-            <footer>
+            <footer className="text-center mt-4">
                 <p>&copy; 2023 Alpaca Image Generator</p>
             </footer>
         </div>
